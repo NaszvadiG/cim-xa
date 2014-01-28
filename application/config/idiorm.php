99e8f00337f963f258109c4848a4ee99d3ca1cea
unknown <sthurber@finpago.com>
2014-01-28 17:23:56 -0500

    config/idiorm.php

---
+++ application/config/idiorm.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	ORM::configure(array(
+		'connection_string' => 'mysql:host=localhost;dbname=test',
+		'username' => 'root',
+		'password' => ''
+	));
+
+?>
