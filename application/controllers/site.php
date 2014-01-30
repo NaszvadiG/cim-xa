b599a78e481dd841addb329f0ab7cf42567ce991
unknown <sthurber@finpago.com>
2014-01-29 19:10:27 -0500

    require

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -6,29 +6,29 @@ function __construct()
 {
 	parent::__construct();	
 	$this->load->helper('security');
-	require_once '/savant/Savant3.php'; // NOT REQUIRED IF ONLY USING CI VIEWS
-	require_once '/idiorm/idiorm.php';
-	require_once '/application/config/idiorm.php';
-	require_once '/application/libraries/Zend/Mail/Message.php'; // EXAMPLE ZEND LIB 
+	require 'savant/Savant3.php'; // NOT REQUIRED IF ONLY USING CI VIEWS
+	require 'idiorm/idiorm.php';
+	require 'application/config/idiorm.php';
+	require 'application/libraries/Zend/Mail/Message.php'; // EXAMPLE ZEND LOAD 
 }		
  
-public function songs1() // CONTROLLER FUNCTION USING IDIORM WITH A SAVANT VIEW
+public function songs1() // CONTROLLER FUNCTION USING IDIORM FOR A SAVANT VIEW
 {
 	$data = new Savant3();
 	$data->songs = ORM::forTable('songs') 
 		->whereRaw('(`time` > ? AND `time` < ?)', array(2, 5)) 
 		->orderByAsc('artist') 
 		->findResultSet();
-	$data->display('songs1.php');
+	$data->display('songs1.php'); // SAVANT VIEW
 }     	
  
-public function songs2() // CONTROLLER FUNCTION USING IDIORM WITH A CODEIGNITER VIEW
+public function songs2() // CONTROLLER FUNCTION USING IDIORM FOR A CODEIGNITER VIEW
 {
 	$data['songs'] = ORM::forTable('songs') 
 		->whereRaw('(`time` > ? AND `time` < ?)', array(2, 5)) 
 		->orderByAsc('artist') 
 		->findResultSet();
-	$this->load->view('songs2.php',$data);
+	$this->load->view('songs2.php', $data); // CODEIGNITER VIEW
 }     		
  
 } // END CLASS
