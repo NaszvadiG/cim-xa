1d09628a63c8e94f33caf009ec999a8218924f80
unknown <sthurber@finpago.com>
2014-02-08 12:16:46 -0500

    tweak

--- application/views/demo.php
+++ application/views/demo.php
@@ -10,43 +10,8 @@ foreach($css_files as $file): ?>
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
-
-<style type='text/css'>
-body
-{
-	font-family: Arial;
-	font-size: 14px;
-}
-a 
-{
-    color: blue;
-    text-decoration: none;
-    font-size: 14px;
-}
-a:hover
-{
-	text-decoration: underline;
-}
-.dataTables_filter
-{
-	display:none;
-}
-</style>
 </head>
 <body>
-<!--	
-<div>
-<a href='<?php echo site_url('demos/customers_management')?>'>Customers</a> |
-<a href='<?php echo site_url('demos/orders_management')?>'>Orders</a> |
-<a href='<?php echo site_url('demos/products_management')?>'>Products</a> |
-<a href='<?php echo site_url('demos/offices_management')?>'>Offices</a> | 
-<a href='<?php echo site_url('demos/employees_management')?>'>Employees</a> |		 
-<a href='<?php echo site_url('demos/film_management')?>'>Films</a> | 
-<a href='<?php echo site_url('demos/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a> | 
-<a href='<?php echo site_url('demos/multigrids')?>'>Multigrid [BETA]</a>
-</div>>
-<div style='height:10px;'></div>
--->  
 <div>
 <?php echo $output; ?>
 </div>
