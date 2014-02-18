307fe279d11fe78f8bfe33b88db5e35250e0b28f
unknown <sthurber@finpago.com>
2014-02-18 18:30:21 -0500

    examples

--- application/controllers/examples.php
+++ application/controllers/examples.php
@@ -198,7 +198,6 @@ class Examples extends CI_Controller {
 	{
 		$crud = new grocery_CRUD();
 
-		$crud->set_theme('datatables');
 		$crud->set_table('employees');
 		$crud->set_relation('officeCode','offices','city');
 		$crud->display_as('officeCode','Office City');
