1e814cdfe29f0dc39b5f15e9581f295269550c5f
unknown <sthurber@finpago.com>
2014-02-20 10:11:34 -0500

    crud fix

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -45,19 +45,7 @@ public function songs1() // CONTROLLER FUNCTION USING IDIORM FOR A SAVANT VIEW
 		->findResultSet(); 
 	$data->display('songs1.php'); // SAVANT VIEW
 }     	
-/* 
-public function songs2() // CONTROLLER FUNCTION USING IDIORM FOR A CODEIGNITER PARSER VIEW
-{
-	$this->load->library('parser');	
-	require 'idiorm/idiorm.php';
-	require 'application/config/idiorm.php';
-	$data['songs'] = ORM::forTable('songs') 
-		->whereRaw('(`time` > ? AND `time` < ?)', array(2, 5)) 
-		->orderByAsc('artist') 
-		->findArray(); // PARSER CLASS ACCEPTS AN ARRAY
-	$this->parser->parse('songs2', $data);
-}     	
-*/
+
 public function songs2() // CONTROLLER FUNCTION USING IDIORM FOR A CODEIGNITER STANDARD VIEW
 {
 	require 'idiorm/idiorm.php';
@@ -79,53 +67,6 @@ public function songs3() // CRUD FUNCTION USING CODEIGNITER'S MYSQLI DATABASE IN
 	$this->load->view('songs3.php', $crud->render());  
 }			
  
-//====MULTIPLE CRUDS: experimental 
-/*
-public function _the_output($output = null)
-{
-	$this->load->view('demo.php',$output);
-}
-
-public function multigrids()
-{
-	$this->config->load('grocery_crud');
-	$this->config->set_item('grocery_crud_dialog_forms',true);
-	$this->config->set_item('grocery_crud_default_per_page',10);
-	$output1 = $this->offices_management2();
-	$output2 = $this->employees_management2();
-	$js_files = $output1->js_files + $output2->js_files;
-	$css_files = $output1->css_files + $output2->css_files;
-	$output = 	'<h3 style="margin-bottom:10px;">Offices</h3>'.$output1->output.
-				'<h3 style="margin-bottom:10px;">Employees</h3>'.$output2->output;
-	$this->_the_output((object)array(
-			'js_files' => $js_files,
-			'css_files' => $css_files,
-			'output' => $output
-	));
-}
-
-public function offices_management2()
-{
-	$this->load->database();
-	$this->load->library('grocery_CRUD');
-	$crud = new grocery_CRUD();
-	$crud->set_table('offices')
-		->unset_print() ->unset_export() ->unset_delete()
-		->set_crud_url_path(site_url(strtolower(__CLASS__."/".__FUNCTION__)),site_url(strtolower(__CLASS__."/multigrids")));
-	if($crud->getState() != 'list') { $this->_the_output($crud->render()); } else { return $crud->render(); }
-}
-
-public function employees_management2()
-{
-	$this->load->database();
-	$this->load->library('grocery_CRUD');
-	$crud = new grocery_CRUD();
-	$crud->set_table('employees')
-		->unset_print() ->unset_export() ->unset_delete()
-		->set_crud_url_path(site_url(strtolower(__CLASS__."/".__FUNCTION__)),site_url(strtolower(__CLASS__."/multigrids")));
-	if($crud->getState() != 'list') { $this->_the_output($crud->render()); } else { return $crud->render(); }
-}
-*/
 } // END Site Class
 
 ?>
