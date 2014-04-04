42b49775446de9178b87542b54155d5b58f0a6f2
unknown <sthurber@finpago.com>
2014-04-03 20:05:59 -0400

    tweak

--- application/views/example.php
+++ application/views/example.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8" />
+<meta charset="utf-8" />
+<link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
 <?php 
 foreach($css_files as $file): ?>
 	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
@@ -9,38 +10,32 @@ foreach($css_files as $file): ?>
 <?php foreach($js_files as $file): ?>
 	<script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
-<style type='text/css'>
-body
-{
-	font-family: Arial;
-	font-size: 14px;
-}
-a {
-    color: blue;
-    text-decoration: none;
-    font-size: 14px;
-}
-a:hover
-{
-	text-decoration: underline;
-}
-</style>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div>
-		<!--a href='<?php echo site_url('examples/customers_management')?>'>Customers</a> 
-		<a href='<?php echo site_url('examples/orders_management')?>'>Orders</a> 
-		<a href='<?php echo site_url('examples/products_management')?>'>Products</a--> 
-		<a href='<?php echo site_url('examples/offices_management')?>'>Offices</a> | 
-		<a href='<?php echo site_url('examples/employees_management')?>'>Employees</a> 		 
-		<!--a href='<?php echo site_url('examples/film_management')?>'>Films</a> 
-		<a href='<?php echo site_url('examples/film_management_twitter_bootstrap')?>'>Twitter Bootstrap Theme [BETA]</a> 
-		<a href='<?php echo site_url('examples/multigrids')?>'>Multigrid [BETA]</a-->
-		
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
 	</div>
-	<div style='height:20px;'></div>  
-    <div>
-		<?php echo $output; ?>
-    </div>
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">		  
+		<ul class="nav navbar-nav">
+			<li><a class="navbar-brand" href="#">Menu:</a></li>
+			<li><a href="<?php echo site_url('examples/offices_management')?>">Offices</a></li>
+			<li><a href="<?php echo site_url('examples/employees_management')?>">Employees</a></li>
+		</ul>
+	</div>
+</div> 
+<div style='height:50px;'></div>  
+
+<div>
+	<?php echo $output; //THIS DISPLAYS THE CRUD ?>
+</div>
+
 </body>
 </html>
